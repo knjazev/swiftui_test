2559360bdf13c0c94f0915e6d7e89aca23435f4c
Dmitry Kniazev <d.knjazev@gmail.com>
2021-08-03 12:04:10 +0300

    Initial Commit

---
+++ Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by UPIT on 3.08.21.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cats
+//
+//  Created by UPIT on 3.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
